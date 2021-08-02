b7099dbcf8b10ebb3c56d6cb1716430f3a229188
Silvina Lorena <silvyhs.campanitah@hotmail.com>
2021-08-02 00:29:09 -0300

    buscador

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View , Image, ScrollView, TextInput} from 'react-native';
+import { Movie } from './components/Movie';
 
 export default function App() {
+
+  const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=600d38b9ea12ad8eed83670ed81d230c&page=1";
+  const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=600d38b9ea12ad8eed83670ed81d230c&query=";
+
+
+  const [movies, setMovies ] = React.useState([]); 
+  const [state, setState] = React.useState({
+     s:"Enter a movie..",
+     results:[],
+     selected:{}
+   });
+  
+  
+  React.useEffect(() => {
+    fetch(FEATURED_API)
+    .then((res) => res.json())
+    .then((data) => {
+            setMovies(data.results);
+    });
+}, []);
+
+const handleOnsubmit = (e) => {
+  e.preventDefault();
+
+  fetch(SEARCH_API + state.s)
+  .then((res) => res.json())
+  .then((data) => {
+    const results= data.results
+    setMovies(data.results)
+  })
+}
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>MOVIE DB API</Text>
+    <TextInput
+    style={styles.searchbox}
+    onChangeText={text => setState(prevState => {
+        return {...prevState, s: text}
+    })}
+    onSubmitEditing={handleOnsubmit}
+    value={state.s}
+    />    
+    <ScrollView style={styles.results}>
+     
+      {movies && movies.map((movie) => <Movie key={movie.id} {...movie}/>)}
+      
+    
+    </ScrollView>
+    
     </View>
+    
   );
-}
+  
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'#22254b',
+    alignItems:'center',
+    justifyContent:'flex-start',
+    paddingTop: 70,
+    paddingHorizontal:20,
   },
-});
+  title:{
+  color:'#FFF',
+  fontSize: 32,
+  fontWeight: '700',
+  textAlign:'center',
+  marginBottom: 20,
+  },
+  searchbox:{
+  fontSize:20,
+  fontWeight:'300',
+  padding: 20,
+  width:'100%',
+  backgroundColor: '#FFF',
+  borderRadius:8,
+  marginBottom:40
+  },
+  results:{
+    flex:1,
+  },
+})
---
+++ components/Movie.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet,Text, View, Image } from 'react-native';
+
+const IMG_API="https://image.tmdb.org/t/p/w1280";
+
+export const Movie = ({title, poster_path, overview, vote_average, release_date}) => {
+
+ 
+
+    return (
+
+    <View style={styles.container,styles.result}>
+        <Image
+         source={{uri: IMG_API+poster_path}}
+         style={{
+          width: 350, height: 500, marginBottom: 10
+        }}
+        resizeMode="cover"
+        />
+        <Text style={styles.heading}>{title}</Text>
+            <Text style={styles.heading}>{release_date}</Text>
+            <Text style={styles.heading}>{vote_average}</Text>
+            <Text style={styles.heading}>Sinopsis</Text>
+            <Text style={styles.heading}>{overview}</Text>
+    </View>
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor:'#22254b',
+      alignItems:'center',
+      justifyContent:'flex-start',
+      paddingTop: 70,
+      paddingHorizontal:20,
+    },
+    title:{
+    color:'#FFF',
+    fontSize: 40,
+    fontWeight: '700',
+    textAlign:'center',
+    marginBottom: 20,
+    },
+    result:{
+      flex:1,
+      width: '100%',
+      marginBottom:20
+    },
+    heading:{
+      color: '#FFF',
+      fontSize:18,
+      fontWeight: '700',
+      padding: 20,
+      backgroundColor:'#445565'
+    }
+
+})
